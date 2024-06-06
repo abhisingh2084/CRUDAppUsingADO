78736b633c8bde2c7eebae6f03a370ceba3ffa85
abhishek kumar <abhisingh2084@gmail.com>
2024-06-06 16:04:25 +0530

    Add project files.

---
+++ CRUDAppUsingADO/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CRUDAppUsingADO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CRUDAppUsingADO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly EmployeeDataAccessLayer dal;
+
+        public HomeController()
+        {
+                dal = new EmployeeDataAccessLayer();
+        }
+
+        public IActionResult Index()
+        {
+            List<Employees> emps = dal.getAllEmployees();
+            return View(emps);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CRUDAppUsingADO/EmployeeDataAccessLayer.cs
@@ -0,0 +1,39 @@
+﻿using CRUDAppUsingADO.Models;
+using System.Data.SqlClient;
+
+
+
+namespace CRUDAppUsingADO
+{
+    public class EmployeeDataAccessLayer
+    {
+        string cs = ConnectionString.dbcs;
+        public List<Employees> getAllEmployees()
+        {
+            List<Employees> empList = new List<Employees>();
+
+            using (SqlConnection con = new SqlConnection(cs))
+
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployee", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Employees emp = new Employees();
+                    emp.Id = Convert.ToInt32(reader["Id"]);
+                    emp.Name = reader["name"].ToString() ?? "";
+                    emp.Gender = reader["gender"].ToString() ?? "";
+                    emp.Age = Convert.ToInt32(reader["age"]);
+                    emp.Designation = reader["designation"].ToString() ?? "";
+                    emp.City = reader["city"].ToString() ?? "";
+                    empList.Add(emp);
+
+                }
+            }
+
+            return empList;
+        }
+    }
+}
